523b7796655cf4bf0166584f79cd911a07b66a96
Aurélien Darbellay <aurelien.darbellay@gmail.com>
2025-02-19 19:19:59 +0100

    queries 1-4

--- playground-1.mongodb.js
+++ playground-1.mongodb.js
@@ -1,4 +1,6 @@
 use('Sprint2Task4');
 db.restaurants.find();
-db.restaurants.find({})
+db.restaurants.find({},{_id: 1, borough: 1, cuisine: 1, restaurant_id: 1})
+db.restaurants.find({},{_id: 0, borough: 1, cuisine: 1, restaurant_id: 1})
+db.restaurants.find({},{_id: 0, borough: 1, address: {zipcode:1}, restaurant_id: 1})
 
